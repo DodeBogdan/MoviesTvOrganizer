3e8b3abb7a06ba29df448fa32305921f6f37b149
Bogdan Gheorghe <dodebogdan@yahoo.com>
2021-01-20 22:28:51 +0200

    Final.

--- app/src/main/java/com/example/moviestvorganizer/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/moviestvorganizer/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Bine ai venit in organizatorul tau de filme!");
     }
 
     public LiveData<String> getText() {
